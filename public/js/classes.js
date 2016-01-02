b317790ef05ba059351d0472560a81c7ffad5d2c
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-01-02 17:34:25 -0500

    update

--- public/js/classes.js
+++ public/js/classes.js
@@ -9,13 +9,14 @@ parameters:
   -endDate: array of strings containing year, month and day 
   -icon: html markup for description / icon
   -tags: array of strings
+  -visible: should this object be rendered
 methods:
-LatLon() -- returns latiude and longitude as openlayers lat lon object
-getFeature() -- returns openlayers feature object for map object
-getTimelineObject -- returns vis object for point
+    LatLon(): returns latiude and longitude as openlayers lat lon object
+    getFeature(): returns openlayers feature object for map object
+    getTimelineObject():  returns vis timeline object for point
+    getDates(): returns dates for object as js dates
 */
 function MapObject(lat, log, title, startDate, endDate, icon, tags, uniqueid) {
-
     //object properties
     this.lat = lat || 0;
     this.lon = log || 0;
@@ -24,18 +25,22 @@ function MapObject(lat, log, title, startDate, endDate, icon, tags, uniqueid) {
     this.endDate = endDate || [];
     this.icon = icon || "";
     this.tags = tags || [];
+    this.visible = true;
     //object methods
     this.LonLat = function() {
         return ol.proj.fromLonLat([this.lon, this.lat]);
     };
     this.getFeature = function() {
+    if(this.visible)
         var iconFeature = new ol.Feature({
-            geometry: new
-            ol.geom.Point(ol.proj.transform([this.lat, this.lon], 'EPSG:4326', 'EPSG:3857')),
+            geometry: new ol.geom.Point(ol.proj.transform([this.lat, this.lon], 'EPSG:4326', 'EPSG:3857')),
             name: this.title,
+            visible: this.visible,
             tags: this.tags,
             icon: this.icon
         });
+    else
+        var iconFeature = false;
         return iconFeature;
     };
     this.getTimelineObject = function() {
@@ -52,19 +57,48 @@ function MapObject(lat, log, title, startDate, endDate, icon, tags, uniqueid) {
         visObject.start = new Date(this.startDate[2],this.startDate[1],this.startDate[0]);
         return visObject;
     };
+    this.getDates = function()  {
+        var returnObject = { start: new Date() };
+        if (!(typeof(this.startDate) === 'undefined')) {
+        //date format = mm/dd/yyyy
+        if( this.startDate.length === 3 )
+            returnObject.start = new Date(this.startDate[2],this.startDate[1],this.startDate[0]);
+        //date format = Wed Mar 25 2015 01:00:00 GMT+0100 (W. Europe Standard Time)
+        else if( this.startDate.length === 1 ) 
+            returnObject.start = new Date(this.startDate[0]);
+        else
+            alert("Error: Unsupported start date format on marker:" + this.title);
+    } else {
+        alert("Error Date undefined on:" + this.title);
+    }  
+        //markers without without an end date are allowed, but don't return an end 
+    if(!(typeof(this.endDate) === 'undefined')) {
+        if( this.endDate.length == 3 )
+            returnObject.end = new Date(this.endDate[2],this.endDate[1],this.endDate[0]);
+        //date format = Wed Mar 25 2015 01:00:00 GMT+0100 (W. Europe Standard Time)
+        else if( this.endDate.length == 1 ) 
+            returnObject.end = new Date(this.endDate[0]);
+        else
+            alert("Error: Unsupported end date format on marker:" + this.title);
+    }
+        return returnObject;
+    };
 }
 /*
 Category Class
 parameters:
   -elements: list of MapObjects
   -icon: url of image to display
-          vectorSource.addFeature(iconFeature);
-
+methods:
+vectorSource() - returns an openlayers3 vector containing the category features
+features() - returns a list containing the category features
+iconStyle() - returns ol style object for class of objects
 */
 function Category(elements) {
     /*==========================================*/
     //variables
     /*==========================================*/
+    this.visible = true;
     this.elements = elements || [];
     this.icon = "http://openlayers.org/en/v3.9.0/examples/data/icon.png";
     /*==========================================*/
@@ -79,7 +113,8 @@ function Category(elements) {
         }); 
         // store the ol features
         for (var i = this.elements.length - 1; i >= 0; i--) {
-            iconFeatures.addFeature(this.elements[i].getFeature());
+            if ( this.elements[i].visible === true )
+                iconFeatures.addFeature(this.elements[i].getFeature());
         };
         return iconFeatures
     };
@@ -88,12 +123,15 @@ function Category(elements) {
     */
     this.features = function() {
         elementFeatures = [];
-        for (var i = this.elements.length - 1; i >= 0; i--)
-            elementFeatures.push(this.elements[i].getFeature());
+        for (var i = this.elements.length - 1; i >= 0; i--) {
+            var feature = this.elements[i].getFeature();
+            if (typeof feature === 'object')
+                 elementFeatures.push(feature);
+        }
         return elementFeatures;
     };
      /*
-    features - returns ol graphic object for category maker
+    iconStyle - returns ol graphic object for category maker
     */
     this.iconStyle = function() {
         //create the style
@@ -103,6 +141,7 @@ function Category(elements) {
                 anchorXUnits: 'fraction',
                 anchorYUnits: 'pixels',
                 opacity: 0.75,
+                visible: this.visible,
                 src: this.icon
             }))
         });
@@ -134,3 +173,83 @@ function Category(elements) {
         return visObjects;
     };
 };
+/*
+Vis Timeline Class
+parameters:
+  -renderobject -- id of div to render map at
+  -objects -- category object or array of category objects
+  -map -- ol3 map object to pair Timeline instance with
+methods:
+*/
+
+function Timeline(renderpoint,objects,map) {
+  /*
+    corresponding map to timeline
+  */
+  this.map = map || {};
+    if ((typeof(renderpoint) === 'undefined') || typeof(objects) === 'undefined') {
+    alert('Error: Invalid timeline parameters');
+    };
+  if (debug) console.log(renderpoint,objects);
+ // DOM element where the Timeline will be attached
+  var container = document.getElementById(String(renderpoint));
+  //load data points
+  var data = [];
+
+  if(objects.constructor === Array) {
+    for (var i = objects.length - 1; i >= 0; i--) {
+      data.push(objects[i].getTimelineObject());
+    };
+    } else {
+      //only a single category of data exists
+      data = objects.getTimelineObject();
+    }
+  /*
+     Begin declaration of class variables
+  */
+  // Create the vis DataSet 
+  this.items = new vis.DataSet(data); 
+  // Configuration for the Timeline
+  this.options = {};
+  this.showCurrentTime = false; // do not display bar at the current date
+  //get first and last dates to set max and min with
+  this.options.min = data[0].start;
+  /*
+      End declaration of class variables
+  */
+  //set visjs timeline maximum and minimum
+  for (var i = data.length - 1; i >= 0; i--) {
+    if ( typeof(data[i].start) !== 'undefined' ) {
+        if ( data[i].start < this.options.min ) 
+               this.options.min = data[i].start;
+    }
+  }
+   /* Construct timeline */
+   this.tm = new vis.Timeline(container, this.items, this.options);
+   /* Add parent object to child for event listeners */
+   this.tm.parent = this;
+   /*  Listen for timeline range changes */
+    this.tm.on('rangechanged', function (properties) {
+        /* set window title */
+        window.document.title = String(properties.start) + " to "+ String(properties.end);
+        if ( typeof(this.parent.map.mapData) === 'undefined' ) { 
+            alert('Error: Invalid Map Object!');
+        } else {
+            console.log(this.parent.map.mapData);
+            for ( var i = this.parent.map.mapData.elements.length - 1; i >= 0; i-- ) {
+                var dates = this.parent.map.mapData.elements[i].getDates();
+                if ( (properties.start < dates.start) || (dates.end > properties.end) )
+                        this.parent.map.mapData.elements[i].visible = true;
+                    else
+                        this.parent.map.mapData.elements[i].visible = false;
+            };
+            console.log(this.parent)
+            this.parent.map.initalize();
+      }
+    })
+    /*  Listen for clicks on timeline elements */
+   this.tm.on('select', function (properties) {
+    });
+  
+};
+
--- public/js/map.js
+++ public/js/map.js
@@ -1,48 +1,75 @@
-var debug = true;
-/*==========================================*/
-/*
-categories -- an array of category objects
-*/  
-var categories = [];
-var WoosterPoints =  new Category([]);
-var myPoint = new MapObject(Math.random()*360-180, Math.random()*180-90, "Myspot", [1,1,1992], [03,02,2011], "place", [], "aaaaa");
-var myPoint2 = new MapObject(Math.random()*360-180, Math.random()*180-90, "Myspot2", [01,01,1940], [03,11,1963], "place2", [], "aa3aa");
+var WoosterPoints = new Category([]);
+var myPoint = new MapObject(Math.random() * 360 - 180, Math.random() * 180 - 90, "Myspot", [1, 1, 1992], [03, 02, 2011], "place", [], "aaaaa");
+var myPoint2 = new MapObject(Math.random() * 360 - 180, Math.random() * 180 - 90, "Myspot2", [01, 01, 1940], [03, 11, 1963], "place2", [], "aa3aa");
 WoosterPoints.add(myPoint2);
 WoosterPoints.add(myPoint);
 /*
-mapLayer -- stores all layers;
-*/
-var mapLayers = [];
+/* */
+var debug = true;
 /*==========================================*/
+function Map(data, renderlocation) {
+    /*
+    renderlocation -- id of div container for map
+    */
+    this.renderlocation = renderlocation || '';
+    /*
+    categories -- an array of category objects
+    */
+    this.categories = [];
+    /*
+    mapLayer -- stores all layers;
+    */
+    this.mapLayers = [];
+    /*
+    The data parameter can either be an array of Categories or a single Category. 
+    */
+    this.mapObjects = [];
+    this.initalize = function() {
+    this.mapObjects = [];
+    //add osm layer
+    this.mapObjects.push(new ol.layer.Tile({
+        source: new ol.source.OSM()
+    })); 
+    if (data.constructor === Array) {
+        for (var i = data.length - 1; i >= 0; i--) {
+            this.mapObjects.push(data[i].vectorLayer());
+        };
+    } else {
+        this.mapObjects.push(data.vectorLayer());
+    }
+    //store a reference to our the orginal data set
+    this.mapData = data;
+    /*==========================================*/
 
-   
 
+    /*==========================================*/
+    //initalize map
+    /*==========================================*/
+    //display only visible map elements
+    if (debug) console.log(data.elements);
+    var m = new ol.Map({
+        layers: this.mapObjects,
+        target: document.getElementById(String(this.renderlocation)),
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 3
+        })
+    });
+    m.on('singleclick', function(evt) {
+        var coordinates = m.getEventCoordinate(evt.originalEvent);
+        console.log(coordinates);
+    });
+           console.log(m.layers);
 
-/*==========================================*/
-//initalize map
-/*==========================================*/
-
-
-mapLayers.push(new ol.layer.Tile({ source: new ol.source.OSM() })); //add osm layer
-mapLayers.push(WoosterPoints.vectorLayer());
-if (debug) console.log(WoosterPoints.vectorLayer());
-    var map = new ol.Map({
-      layers: mapLayers,
-      target: document.getElementById('map'),
-      view: new ol.View({
-        center: [0, 0],
-        zoom: 3
-      })
-    }); 
-
-map.on('singleclick', function(evt) {
-            var coordinates = map.getEventCoordinate(evt.originalEvent);
-            console.log(coordinates);
-});
-/*==========================================*/
-//
-// Define rotate to north control.
-//
-
-
-
+    this.update();
+    /*==========================================*/
+    //update() -- redraws map
+    /*==========================================*/
+  };
+    this.update = function() {
+       // this.mapLayer = map;
+        console.log('updated!')
+    }
+}
+var map = new Map(WoosterPoints, 'map');
+map.initalize();
